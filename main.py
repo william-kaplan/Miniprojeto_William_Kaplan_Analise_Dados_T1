79600535805ffdba17b36ff4b6276714f1216d0e
William Kaplan <125420246+william-kaplan@users.noreply.github.com>
2026-05-30 19:45:57 -0300

    correcao na leitura e informacoes inicias
    Correção na leitura do arquivo, pois está separado com ; 
    Removi o import do display, pois imprimir no terminal do vscode não tem diferença entre print e display.
    Exibi as informações inicias do Dataset.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pandas as pd
-from IPython.display import display
 
-df = pd.read_csv('base_varejo.csv')
+df = pd.read_csv('base_varejo.csv', sep=';')
+print('Dataset carregado com sucesso.')
+print('-'*50)
+
+print('Número de linhas e colunas do Dataset:', df.shape)
+print('-'*50)
+
+print('Tipos de dados de cada coluna do Dataset:')
+print(df.dtypes)
+print('-'*50)
+
+print('Nome das coluna do Dataset:')
+print(df.columns)
+print('-'*50)
+
+print('Primeiras linhas do Dataset:')
+print(df.head(10))
+print('-'*50)
