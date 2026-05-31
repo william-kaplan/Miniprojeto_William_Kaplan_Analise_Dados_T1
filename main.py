d878d3e4ba69497a061639088ad42c38d01d7631
William Kaplan <125420246+william-kaplan@users.noreply.github.com>
2026-05-30 21:56:53 -0300

    limpeza, conversao e comentarios
    Adicionei comentários no código para melhor entendimento.
    Realizei a limpeza dos dados, removendo as colunas 100% nulas e os dados duplicados. Verificando se algum item da coluna de categoria está nula, se sim alterando para Sem Categoria.
    Convertendo o tipo de dado da data se str para datetime.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+# importando biblioteca
 import pandas as pd
 
+# lendo o arquivo csv, que está separado por ;
 df = pd.read_csv('base_varejo.csv', sep=';')
 print('Dataset carregado com sucesso.')
 print('-'*50)
 
+# exibindo principais informações do Dataset
 print('Número de linhas e colunas do Dataset:', df.shape)
 print('-'*50)
 
@@ -17,4 +20,35 @@ print('-'*50)
 
 print('Primeiras linhas do Dataset:')
 print(df.head(10))
+print('-'*50)
+
+print('Total de valores nulos por coluna:')
+print(df.isnull().sum())
+print('-'*50)
+
+print('Total de valores duplicados:', df.duplicated().sum())
+print('-'*50)
+
+# Iniciando as 'limpezas' e copiando o Dataset para um novo
+df_limpa = df.copy()
+
+# verificando se tem valores nulos na coluna PR_CAT se tiver alterar para 'Sem Categoria'
+df_limpa['PR_CAT'] = df_limpa['PR_CAT'].apply(lambda x: 'Sem Categoria' if pd.isna(x) else x)
+
+# convertendo o tipo da coluna de data
+df_limpa['DATA'] = pd.to_datetime(df_limpa['DATA'], dayfirst=True, errors='coerce')
+
+# removendo as colunas que estão 100% vazias
+df_limpa = df_limpa.dropna(axis=1, how='all')
+print('Novo total de valores nulos por coluna:')
+print(df_limpa.isnull().sum())
+print('-'*50)
+
+# removendo valores duplicados
+df_limpa = df_limpa.drop_duplicates()
+print('Novo total de valores duplicados:', df_limpa.duplicated().sum())
+print('-'*50)
+
+print('Tipos de dados de cada coluna do Dataset após conversão e limpeza:')
+print(df_limpa.dtypes)
 print('-'*50)
